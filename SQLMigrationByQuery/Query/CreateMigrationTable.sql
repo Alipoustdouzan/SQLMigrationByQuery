02727195b33af4ddbbd67aee1fddd8e4a5fac80d
AliPoustDouzan <AliPoustDouzan@gmail.com>
2021-03-08 10:56:09 +0330

    Release 1.0.0.13

--- SQLMigrationByQuery/Query/CreateMigrationTable.sql
+++ SQLMigrationByQuery/Query/CreateMigrationTable.sql
@@ -18,38 +18,40 @@ BEGIN
     ADD
         CONSTRAINT [PK____DatabaseMigration] PRIMARY KEY CLUSTERED([intMigrationID])
 
-   CREATE UNIQUE NONCLUSTERED INDEX [IX____DatabaseMigration] ON [dbo].[___DatabaseMigration]
-(
-	[strMigrationProject] ASC,
-	[strMigrationName] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+    CREATE UNIQUE NONCLUSTERED INDEX [IX____DatabaseMigration]
+    ON [dbo].[___DatabaseMigration]([strMigrationProject] ASC, [strMigrationName] ASC)
+    WITH(PAD_INDEX=OFF, STATISTICS_NORECOMPUTE=OFF, SORT_IN_TEMPDB=OFF, IGNORE_DUP_KEY=OFF, DROP_EXISTING=OFF, ONLINE=OFF, ALLOW_ROW_LOCKS=ON, ALLOW_PAGE_LOCKS=ON)
+    ON [PRIMARY]
 END
 ELSE
 BEGIN
-    IF(EXISTS (
-              SELECT
-                  *
-              FROM INFORMATION_SCHEMA.COLUMNS
-              WHERE TABLE_NAME='___DatabaseMigration'
-                    AND COLUMN_NAME='strMigrationProject'
-                    AND CHARACTER_MAXIMUM_LENGTH=50
-              )
+    IF(
+      EXISTS (
+             SELECT
+                 *
+             FROM INFORMATION_SCHEMA.COLUMNS
+             WHERE TABLE_NAME='___DatabaseMigration'
+                   AND COLUMN_NAME='strMigrationProject'
+                   AND CHARACTER_MAXIMUM_LENGTH<200
+             )
+      OR EXISTS (
+                SELECT
+                    *
+                FROM INFORMATION_SCHEMA.COLUMNS
+                WHERE TABLE_NAME='___DatabaseMigration'
+                      AND COLUMN_NAME='strMigrationName'
+                      AND CHARACTER_MAXIMUM_LENGTH<300
+                )
       )
     BEGIN
+        DROP INDEX [IX____DatabaseMigration] ON [dbo].[___DatabaseMigration]
         ALTER TABLE dbo.___DatabaseMigration
         ALTER COLUMN strMigrationProject [VARCHAR](200) COLLATE Persian_100_CI_AS NOT NULL
-    END
-    IF(EXISTS (
-              SELECT
-                  *
-              FROM INFORMATION_SCHEMA.COLUMNS
-              WHERE TABLE_NAME='___DatabaseMigration'
-                    AND COLUMN_NAME='strMigrationName'
-                    AND CHARACTER_MAXIMUM_LENGTH=50
-              )
-      )
-    BEGIN
         ALTER TABLE dbo.___DatabaseMigration
         ALTER COLUMN strMigrationName [VARCHAR](300) COLLATE Persian_100_CI_AS NOT NULL
+        CREATE UNIQUE NONCLUSTERED INDEX [IX____DatabaseMigration]
+        ON [dbo].[___DatabaseMigration]([strMigrationProject] ASC, [strMigrationName] ASC)
+        WITH(PAD_INDEX=OFF, STATISTICS_NORECOMPUTE=OFF, SORT_IN_TEMPDB=OFF, IGNORE_DUP_KEY=OFF, DROP_EXISTING=OFF, ONLINE=OFF, ALLOW_ROW_LOCKS=ON, ALLOW_PAGE_LOCKS=ON)
+        ON [PRIMARY]
     END
 END
